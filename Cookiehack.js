543994f76a4e8e24e4a6046b5fc6bb83bf05d758
Dawidek484 <Arkowiec666@gmail.com>
2014-12-14 14:33:07 +0000

    Update Cookiehack.js

--- Cookiehack.js
+++ Cookiehack.js
@@ -1,1 +1,2 @@
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 (function($) { $(document).ready(function() { setInterval(function() { $(‘bigCookie’').click(); }, 0.1) }); })(jQuery);
